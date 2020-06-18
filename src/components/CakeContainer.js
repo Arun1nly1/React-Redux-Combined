d826809bbc746f90de4822f870410b4b9e1aa83f
Arun Chapagain <arunsansar123@gmail.com>
2020-06-18 18:54:25 +0545

    React and Redux combined

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <h1>Number of cakes</h1>
+            <button>Buy  Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
