2d4b707a5f953db1693347b2721f23bd31d498b6
Arun Chapagain <arunsansar123@gmail.com>
2020-06-19 17:22:52 +0545

    Upto connect function from react redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import {Provider} from 'react-redux'
+import store from './redux/Store'
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
      <CakeContainer/>
     </div>
+    </Provider>
   );
 }
 
--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { buyCake } from '../redux'
+import {connect} from 'react-redux'
 
-const CakeContainer = () => {
+
+const CakeContainer = (props) => {
     return (
         <div>
-            <h1>Number of cakes</h1>
-            <button>Buy  Cake</button>
+            <h1>Number of cakes - {props.numOfCakes}</h1>
+            <button onClick={props.buyCake}>Buy  Cake</button>
         </div>
     )
 }
 
-export default CakeContainer;
+const mapStateToProps = state =>{
+    return{
+        numOfCakes : state.numOfCakes 
+    }
+}
+
+
+const mapDispatchToProps = dispatch =>{
+    return{
+        buyCake: () =>dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (CakeContainer);
